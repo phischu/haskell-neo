652ae2756e1270ce4b4e68e2cf6e01422a276f12
Philipp Schuster <pschuster@uni-koblenz.de>
2013-07-15 17:27:57 +0200

    one prototypical function

--- src/Web/Neo.hs
+++ src/Web/Neo.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Web.Neo (
     ) where
 
-import Web.Rest ()
+import Web.Rest (
+    RestT,rest,
+    Request(Request),Method(POST))
 
-
+newNode :: (Monad m) => RestT m String
+newNode = do
+    result <- rest (Request POST "/db/data/node" "application/json" "application/json" "")
+    return (show result)
