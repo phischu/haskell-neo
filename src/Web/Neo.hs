689d7b7b5e19a819a140b9aa5db0ed6d94f7a8d1
Philipp Schuster <pschuster@uni-koblenz.de>
2013-07-20 17:13:37 +0200

    giant refactoring

--- src/Web/Neo.hs
+++ src/Web/Neo.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE OverloadedStrings, StandaloneDeriving #-}
-module Web.Neo (
-    ) where
+module Web.Neo where
 
 import Web.Rest (
     RestT,rest,
@@ -9,17 +8,16 @@ import Web.Rest (
     Response(code,responseType,responseBody),
     ResponseCode)
 
-import Control.Error (EitherT,runEitherT,left,tryRead)
+import Control.Error (EitherT,runEitherT,left,readErr)
 
 import Data.Aeson (
-    Value,Object,
+    Value,
     ToJSON,object,(.=),encode,
     FromJSON(parseJSON),
-    withObject,withText,withArray,
+    withObject,withText,
     (.:),eitherDecode)
 import Data.Aeson.Types (Parser)
 
-import Control.Monad (mzero,(>=>))
 import Control.Monad.Trans (lift)
 import Control.Monad.IO.Class (MonadIO)
 
@@ -27,16 +25,12 @@ import Data.Text (Text,append,pack,unpack)
 import qualified  Data.Text as Text (takeWhile,reverse)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BL (toStrict,fromStrict)
-import qualified Data.Vector as Vector (toList)
 import Data.HashMap.Strict (HashMap)
 
 data NeoError = ResponseCodeError ResponseCode Body
               | ResponseTypeError (Maybe ContentType) Body
-              | CreationResponseParseError String
+              | ResponseParseError String
               | ExtractIdError Text
-              | EdgesResponseParseError String
-              | EdgeInfoResponseParseError String
-              | NodeLabelsResponseParseError String
 
 deriving instance Show NeoError
 
@@ -48,98 +42,54 @@ runNeoT hostname port = runRestT hostname port . runEitherT
 defaultRunNeoT :: (MonadIO m) => NeoT m a -> m (Either RestError (Either NeoError a))
 defaultRunNeoT = runNeoT "localhost" 7474
 
-data Node = Node Integer
+data Node = Node {
+    nodeId :: Integer,
+    nodeData :: Properties}
 
 deriving instance Show Node
 
-data Edge = Edge Integer
+instance FromJSON Node where
+    parseJSON = withObject "NodeObject" (\o -> do
+        selfid <- o .: "self" >>= parseSelfId
+        nodedata <- o .: "data"
+        return (Node selfid nodedata))
+
+data Edge = Edge {
+    edgeId :: Integer,
+    edgeStart :: Integer,
+    edgeEnd :: Integer,
+    edgeType :: Label,
+    edgeData :: Properties}
 
 deriving instance Show Edge
 
+instance FromJSON Edge where
+    parseJSON = withObject "EdgeObject" (\o -> do
+        selfid <- o .: "self" >>= parseSelfId
+        startid <- o .: "start" >>= parseSelfId
+        endid   <- o .: "end" >>= parseSelfId
+        label <- o .: "type" >>= parseJSON
+        edgedata <- o .: "data"
+        return (Edge selfid startid endid label edgedata))
+
 type Properties = HashMap Text Value
-
-data CreationResponse = CreationResponse Text
-
-instance FromJSON CreationResponse where
-    parseJSON = extractSelfURI >=> return . CreationResponse
-
 type Label = Text
 
-create :: (Monad m) => Request -> NeoT m CreationResponse
-create request = do
-
-    response <- lift (rest request)
-
-    assertResponseCode (2,0,1) response
-    assertResponseType jsoncontent response
-
-    either (left . CreationResponseParseError) return (strictEitherDecode (responseBody response))
-
-extractId :: (Monad m) => Text -> EitherT NeoError m Integer
-extractId uri = do
-
-    let lastURIsegment = Text.reverse (Text.takeWhile (/= '/') (Text.reverse uri))
-
-    tryRead (ExtractIdError lastURIsegment) (unpack lastURIsegment)
-
-extractTextField :: Text -> Value -> Parser Text
-extractTextField key = withObject "ResponseObject" (\o -> o .: key >>= withText "TextField" return)
-
-extractSelfURI :: Value -> Parser Text
-extractSelfURI = extractTextField "self"
-
-jsonRequest :: Method -> Location -> Body -> Request
-jsonRequest method location body = Request method location jsoncontent jsoncontent body
-
-jsonGetRequest :: Location -> Request
-jsonGetRequest location = jsonRequest GET location ""
-
 newNode :: (Monad m) => NeoT m Node
-newNode = do
-
-    let newNodeRequest = jsonRequest POST "/db/data/node" ""
-
-    CreationResponse selfUri <- create newNodeRequest
-
-    nodeId <- extractId selfUri
-
-    return (Node nodeId)
-
-nodeURI :: Node -> Location
-nodeURI (Node nodeid) = "/db/data/node/" `append` (pack (show nodeid))
-
-edgeURI :: Edge -> Location
-edgeURI (Edge edgeid) = "/db/data/relationship/" `append` (pack (show edgeid))
+newNode = call (jsonRequest POST "/db/data/node" "") (2,0,1)
 
 newEdge :: (Monad m) => Label -> Node -> Node -> NeoT m Edge
-newEdge label sourcenode targetnode = do
-
-    let sourceuri = (nodeURI sourcenode `append` "/relationships")
-        payload   = object [
-            "to"   .= nodeURI targetnode,
-            "type" .= label]
-        newEdgeRequest = jsonRequest POST sourceuri (strictEncode payload)
-
-    CreationResponse selfUri <- create newEdgeRequest
-
-    edgeId <- extractId selfUri
-
-    return (Edge edgeId)
+newEdge label sourcenode targetnode = call newEdgeRequest (2,0,1) where
+    newEdgeRequest = jsonRequest POST sourceuri (strictEncode payload)
+    sourceuri      = (nodeURI sourcenode `append` "/relationships")
+    payload        = object [
+        "to"   .= nodeURI targetnode,
+        "type" .= label]
 
 setProperty :: (Monad m) => Text -> Value -> Location -> NeoT m ()
-setProperty key value uri = do
-
-    let requestUri = uri `append` "/properties/" `append` key
-        setPropertyRequest = jsonRequest PUT requestUri (strictEncode value)
-
-    requestWithEmptyResponse setPropertyRequest
-
-requestWithEmptyResponse :: (Monad m) => Request -> NeoT m ()
-requestWithEmptyResponse request = do
-
-    response <- lift (rest request)
-
-    assertResponseCode (2,0,4) response
+setProperty key value uri = emptyCall setPropertyRequest where
+    requestUri         = uri `append` "/properties/" `append` key
+    setPropertyRequest = jsonRequest PUT requestUri (strictEncode value)
 
 setNodeProperty :: (Monad m) => Text -> Value -> Node -> NeoT m ()
 setNodeProperty key value node = setProperty key value (nodeURI node) 
@@ -148,85 +98,32 @@ setEdgeProperty :: (Monad m) => Text -> Value -> Edge -> NeoT m ()
 setEdgeProperty key value edge = setProperty key value (edgeURI edge)
 
 addNodeLabel :: (Monad m) => Label -> Node -> NeoT m ()
-addNodeLabel label node = do
-
-    let addNodeLabelRequest = jsonRequest POST (nodeURI node `append` "/labels") (strictEncode label)
-
-    requestWithEmptyResponse addNodeLabelRequest
-
-data EdgesResponse = EdgesResponse [Text]
-
-instance FromJSON EdgesResponse where
-    parseJSON = withArray "EdgesResponse" (\edges -> do
-        edgeuris <- mapM extractSelfURI (Vector.toList edges)
-        return (EdgesResponse edgeuris))
+addNodeLabel label node = emptyCall addNodeLabelRequest where
+    addNodeLabelRequest = jsonRequest POST (nodeURI node `append` "/labels") (strictEncode label)
 
 edges :: (Monad m) => Node -> NeoT m [Edge]
-edges node = do
+edges node = call (jsonGetRequest (nodeURI node `append` "/relationships/all")) (2,0,0)
 
-    let allEdgesRequest = jsonGetRequest (nodeURI node `append` "/relationships/all")
-
-    response <- lift (rest allEdgesRequest)
-
-    assertResponseCode (2,0,0) response
-    assertResponseType jsoncontent response
-
-    EdgesResponse edgeuris <- (strictEitherDecode (responseBody response))
-        `whenLeft` EdgesResponseParseError
-
-    edgeids <- mapM extractId edgeuris
-
-    return (map Edge edgeids)
-
-whenLeft :: (Monad m) => Either a b -> (a -> e) -> EitherT e m b
-whenLeft e f = either (left . f) return e
-
-data EdgeInfo = EdgeInfo {edgeInfoStartUri :: Text,
-                          edgeInfoEndUri   :: Text,
-                          edgeInfoType     :: Text,
-                          edgeInfoData     :: Properties}
-
-instance FromJSON EdgeInfo where
-    parseJSON v = do
-        edgeinfostart <- extractTextField "start" v
-        edgeinfoend   <- extractTextField "end" v
-        edgeinfotype  <- extractTextField "type" v
-        edgeinfodata  <- withObject "EdgeInfoObject" (\o -> o .: "data") v
-        return (EdgeInfo edgeinfostart edgeinfoend edgeinfotype edgeinfodata)
-
-edgeInfo :: (Monad m) => Edge -> NeoT m EdgeInfo
-edgeInfo edge = do
-
-    response <- lift (rest (jsonGetRequest (edgeURI edge)))
-
-    assertResponseCode (2,0,0) response
-    assertResponseType jsoncontent response
-
-    (strictEitherDecode (responseBody response))
-        `whenLeft` EdgeInfoResponseParseError
+nodeById :: (Monad m) => Integer -> NeoT m Node
+nodeById nodeid = call (jsonGetRequest ("/db/data/node/" `append` (pack (show nodeid)))) (2,0,0)
 
 source :: (Monad m) => Edge -> NeoT m Node
-source = edgeInfo >=> extractId . edgeInfoStartUri >=> return . Node
+source = nodeById . edgeStart
 
 target :: (Monad m) => Edge -> NeoT m Node
-target = edgeInfo >=> extractId . edgeInfoEndUri >=> return . Node
+target = nodeById . edgeEnd
 
 edgeLabel :: (Monad m) => Edge -> NeoT m Label
-edgeLabel = edgeInfo >=> return . edgeInfoType
+edgeLabel = return . edgeType
 
 edgeProperties :: (Monad m) => Edge -> NeoT m Properties
-edgeProperties = edgeInfo >=> return . edgeInfoData
+edgeProperties = return . edgeData
 
 nodeLabels :: (Monad m) => Node -> NeoT m [Label]
-nodeLabels node = do
+nodeLabels node = call (jsonGetRequest (nodeURI node `append` "/labels")) (2,0,0)
 
-    response <- lift (rest (jsonGetRequest (nodeURI node `append` "/labels")))
-
-    assertResponseCode (2,0,0) response
-    assertResponseType jsoncontent response
-
-    strictEitherDecode (responseBody response)
-        `whenLeft` NodeLabelsResponseParseError
+nodeProperties :: (Monad m) => Node -> NeoT m Properties
+nodeProperties = return . nodeData
 
 assert :: (Monad m) => Bool -> NeoError -> NeoT m ()
 assert True  _        = return ()
@@ -250,3 +147,42 @@ strictEncode = BL.toStrict . encode
 
 strictEitherDecode :: (FromJSON a) => ByteString -> Either String a
 strictEitherDecode = eitherDecode . BL.fromStrict
+
+call :: (FromJSON a,Monad m) => Request -> ResponseCode -> NeoT m a
+call request expectedCode = do
+
+    response <- lift (rest request)
+
+    assertResponseCode expectedCode response
+    assertResponseType jsoncontent response
+
+    parseResponse response
+
+emptyCall :: (Monad m) => Request -> NeoT m ()
+emptyCall request =  lift (rest request) >>= assertResponseCode (2,0,4)
+
+parseResponse :: (FromJSON a,Monad m) => Response -> NeoT m a
+parseResponse response = case strictEitherDecode (responseBody response) of
+    Left errormessage -> left (ResponseParseError errormessage)
+    Right result      -> return result
+
+parseSelfId :: Value -> Parser Integer
+parseSelfId = withText "URI" (\s -> case idSlug s of
+    Left errormessage -> fail errormessage
+    Right idslug      -> return idslug)
+
+idSlug :: Text -> Either String Integer
+idSlug uri = readErr ("Reading URI slug failed: " ++ uriSlug) uriSlug where
+    uriSlug = unpack (Text.reverse (Text.takeWhile (/= '/') (Text.reverse uri)))
+
+jsonRequest :: Method -> Location -> Body -> Request
+jsonRequest method location body = Request method location jsoncontent jsoncontent body
+
+jsonGetRequest :: Location -> Request
+jsonGetRequest location = jsonRequest GET location ""
+
+nodeURI :: Node -> Location
+nodeURI node = "/db/data/node/" `append` (pack (show (nodeId node)))
+
+edgeURI :: Edge -> Location
+edgeURI edge = "/db/data/relationship/" `append` (pack (show (edgeId edge)))
